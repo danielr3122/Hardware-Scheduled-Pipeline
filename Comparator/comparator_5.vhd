91114ad831b002acd5c370d9757474a52bd8c366
danielr3122 <drosenhamer@gmail.com>
2022-12-10 19:54:50 -0600

    each bit

--- Comparator/comparator_5.vhd
+++ Comparator/comparator_5.vhd
@@ -18,7 +18,15 @@ end comparator_5;
 
 architecture mixed of comparator_5 is
 
+    signal a, b, c, d, e : std_logic;
+
     begin
-        o_o <= not(i_d0 xor i_d1);
+        a <= i_d0(0) xor i_d1(0);
+        b <= i_d0(1) xor i_d1(1);
+        c <= i_d0(2) xor i_d1(2);
+        d <= i_d0(3) xor i_d1(3);
+        e <= i_d0(4) xor i_d1(4);
+
+        o_o <= not(a or b or c or d or e);
 
 end mixed;
