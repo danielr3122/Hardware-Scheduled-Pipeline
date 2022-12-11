c9ed487d1174b0b6b51c9b51b453313ce10ce2a6
danielr3122 <drosenhamer@gmail.com>
2022-12-10 23:51:16 -0600

    typo

--- ForwardingUnit/ForwardingUnit.vhd
+++ ForwardingUnit/ForwardingUnit.vhd
@@ -136,7 +136,7 @@ architecture structural of ForwardingUnit is
         cond6 <= (i_BranchSel and eq8);
 
         with (cond5 & cond6) select
-            o_muxReadData2Sel <= b"10" when b"10",
+            o_muxReadData1Sel <= b"10" when b"10",
                                  b"01" when b"01",
                                  b"00" when others;
 
@@ -157,7 +157,7 @@ architecture structural of ForwardingUnit is
         cond8 <= (i_BranchSel and eq10);
 
         with (cond7 & cond8) select
-            o_muxReadData1Sel <= b"10" when b"10",
+            o_muxReadData2Sel <= b"10" when b"10",
                                  b"01" when b"01",
                                  b"00" when others;
 
