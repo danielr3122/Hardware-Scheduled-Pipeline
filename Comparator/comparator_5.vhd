823de4e105ef665ebdbb7b3d22e69491c8e8f47d
danielr3122 <drosenhamer@gmail.com>
2022-12-10 19:48:20 -0600

    comparator

---
+++ Comparator/comparator_5.vhd
@@ -0,0 +1,23 @@
+-------------------------------------------------------------------------
+-- Daniel Rosenhamer
+-------------------------------------------------------------------------
+-- comparator_5.vhd
+-------------------------------------------------------------------------
+-- DESCRIPTION: This file contains an implementation of our compatator 
+--              unit.
+------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity comparator_5 is
+    port(i_d0 : in std_logic_vector(4 downto 0);
+         i_d1 : in std_logic_vector(4 downto 0);
+         o_o  : out std_logic);
+end comparator_5;
+
+architecture mixed of comparator_5 is
+
+    o_o <= i_d0 = i_d1;
+
+end mixed;
