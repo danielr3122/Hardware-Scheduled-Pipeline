cf48c036baa1c16040d9b5227aa8e4ad05a81578
rude87 <rude87@iastate.edu>
2022-12-11 14:49:44 -0600

    created tb

--- Comparator/tb_gen_comparator_32.vhd
+++ Comparator/tb_gen_comparator_32.vhd
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------
 -- Brayton Rude
 -------------------------------------------------------------------------
--- tb_gen_comparator_5.vhd
+-- tb_gen_comparator_32.vhd
 -------------------------------------------------------------------------
 -- DESCRIPTION: This file contains a simple VHDL testbench for the
 -- comparator unit.
@@ -14,27 +14,27 @@ library std;
 use std.env.all;                -- For hierarchical/external signals
 use std.textio.all;             -- For basic I/O
 
-entity tb_gen_comparator_5 is
+entity tb_gen_comparator_32 is
 
     generic(gCLK_HPER   : time := 50 ns);
 
-end tb_gen_comparator_5;
+end tb_gen_comparator_32;
 
-architecture mixed of tb_gen_comparator_5 is
+architecture mixed of tb_gen_comparator_32 is
   
     constant cCLK_PER  : time := gCLK_HPER * 2;
 
-    component gen_comparator_5 is
-        port(i_d0 : in std_logic_vector(4 downto 0);
-             i_d1 : in std_logic_vector(4 downto 0);
+    component gen_comparator_32 is
+        port(i_d0 : in std_logic_vector(31 downto 0);
+             i_d1 : in std_logic_vector(31 downto 0);
              o_o  : out std_logic);
     end component;
 
-    signal s_d0, s_d1 : std_logic_vector(4 downto 0) := b"00000";
+    signal s_d0, s_d1 : std_logic_vector(31 downto 0) := x"00000000";
     signal s_o : std_logic;
     
     begin
-        DUT0: gen_comparator_5
+        DUT0: gen_comparator_32
         port map(i_d0 => s_d0,
                  i_d1 => s_d1,
                  o_o  => s_o);
@@ -43,20 +43,20 @@ architecture mixed of tb_gen_comparator_5 is
         begin
             wait for gCLK_HPER/2;
 
-            s_d0 <= b"00000";
-            s_d1 <= b"00000";
+            s_d0 <= x"00000000";
+            s_d1 <= x"00000000";
             wait for gCLK_HPER/2;
 
-            s_d0 <= b"00001";
-            s_d1 <= b"00000";
+            s_d0 <= x"00000001";
+            s_d1 <= x"00000000";
             wait for gCLK_HPER/2;
 
-            s_d0 <= b"01101";
-            s_d1 <= b"01101";
+            s_d0 <= x"1234BEEF";
+            s_d1 <= x"1234BEEF";
             wait for gCLK_HPER/2;
 
-            s_d0 <= b"10101";
-            s_d1 <= b"01010";
+            s_d0 <= x"00000000";
+            s_d1 <= x"0000FFFF";
             wait for gCLK_HPER/2;
 
     end process;
