0ef6557d01d1e607defbcffb1581624fe1c01104
danielr3122 <drosenhamer@gmail.com>
2022-12-11 20:59:20 -0600

    new test

---
+++ Pipeline Registers/tb_pipeline.vhd
@@ -0,0 +1,185 @@
+-------------------------------------------------------------------------
+-- Brayton Rude
+-- rude87@iastate.edu
+-------------------------------------------------------------------------
+-- tb_pipeline.vhd
+-------------------------------------------------------------------------
+-- DESCRIPTION: This file contains a testbench for the MIPS Register
+--              File which utilizes 32 32bit registers.
+--              
+-- 09/22/2022 by BR::Design created.
+-------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb_pipeline is 
+    generic(gCLK_HPER   : time := 50 ns);
+end tb_pipeline;
+
+architecture behavior of tb_pipeline is
+  
+    -- Calculate the clock period as twice the half-period
+    constant cCLK_PER  : time := gCLK_HPER * 2;
+
+    component IF_ID_Register is
+        generic(N   : integer := 32);
+        port(i_CLK              : in std_logic;
+             i_RST              : in std_logic;
+             i_WE               : in std_logic;
+             i_IF_Inst          : in std_logic_vector(N-1 downto 0);
+             i_IF_PCNext        : in std_logic_vector(N-1 downto 0);
+             o_ID_Inst          : out std_logic_vector(N-1 downto 0);
+             o_ID_PCNext        : out std_logic_vector(N-1 downto 0));
+    end component;
+
+    component ID_EX_Register is
+        port(i_CLK                  : in std_logic;
+             i_RST                  : in std_logic;
+             i_WE                   : in std_logic;
+    
+             i_ID_PCNext            : in std_logic_vector(31 downto 0);
+             i_ID_Halt              : in std_logic;
+             i_ID_DMemWr            : in std_logic;
+             i_ID_Write_Data_Sel    : in std_logic_vector(1 downto 0);
+             i_ID_ALUsrc            : in std_logic;
+             i_ID_ShiftType         : in std_logic_vector(1 downto 0);
+             i_ID_ALUop             : in std_logic_vector(3 downto 0);
+             i_ID_ALUslt            : in std_logic;
+             i_ID_nAdd_Sub          : in std_logic;
+             i_ID_UnsignedSelect    : in std_logic;
+             i_ID_RegWr             : in std_logic;
+             i_ID_JumpInstr         : in std_logic;
+             i_ID_RegDest           : in std_logic_vector(1 downto 0);
+             i_ID_Inst              : in std_logic_vector(31 downto 0);
+             i_ID_extendedImm       : in std_logic_vector(31 downto 0);
+             i_ID_readData1         : in std_logic_vector(31 downto 0);
+             i_ID_readData2         : in std_logic_vector(31 downto 0);
+    
+             o_EX_PCNext            : out std_logic_vector(31 downto 0);
+             o_EX_Halt              : out std_logic;
+             o_EX_DMemWr            : out std_logic;
+             o_EX_Write_Data_Sel    : out std_logic_vector(1 downto 0);
+             o_EX_RegWr             : out std_logic;
+             o_EX_JumpInstr         : out std_logic;
+             o_EX_readData1         : out std_logic_vector(31 downto 0);
+             o_EX_readData2         : out std_logic_vector(31 downto 0);
+             o_EX_extendedImm       : out std_logic_vector(31 downto 0);
+             o_EX_ALUsrc            : out std_logic;
+             o_EX_ShiftType         : out std_logic_vector(1 downto 0);
+             o_EX_ALUop             : out std_logic_vector(3 downto 0);
+             o_EX_ALUslt            : out std_logic;
+             o_EX_nAdd_Sub          : out std_logic;
+             o_EX_UnsignedSelect    : out std_logic;
+             o_EX_RegDest           : out std_logic_vector(1 downto 0);
+             o_EX_Inst              : out std_logic_vector(31 downto 0));
+    end component;
+
+    signal s_IF_RST, s_IF_WE, s_IF_Flush, s_IF_Stall : std_logic;
+
+    signal s_IF_Inst, s_IF_PCNext : std_logic_vector(31 downto 0);
+
+    signal s_ID_RST, s_ID_WE, s_ID_Flush, s_ID_Stall : std_logic;
+
+    signal s_ID_PCNext, s_ID_Inst, s_ID_extImm, s_ID_readData1, s_ID_readData2 : std_logic_vector(31 downto 0);
+
+    signal s_ID_Halt, s_ID_DMemWr, s_ID_ALUsrc, s_ID_ALUslt, s_ID_nAdd_Sub, s_ID_UnsignedSel, s_ID_RegWr, s_ID_JumpInstr : std_logic;
+
+    signal s_ID_Write_Data_Sel, s_ID_RegDest, s_ID_ShiftType : std_logic_vector(1 downto 0);
+
+    signal s_ID_ALUop : std_logic_vector(3 downto 0);
+
+    signal s_EX_PCNext, s_EX_readData1, s_EX_readData2, s_EX_extImm, s_EX_Inst : std_logic_vector(31 downto 0);
+
+    signal s_EX_Halt, s_EX_DMemWr, s_EX_ALUsrc, s_EX_ALUslt, s_EX_nAdd_Sub, s_EX_UnsignedSel, s_EX_RegWr, s_EX_JumpInstr : std_logic;
+    
+    signal s_EX_Write_Data_Sel, s_EX_RegDest, s_EX_ShiftType : std_logic_vector(1 downto 0);
+
+    signal s_EX_ALUop : std_logic_vector(3 downto 0);
+
+
+
+begin
+
+    -- This process sets the clock value (low for gCLK_HPER, then high
+    -- for gCLK_HPER). Absent a "wait" command, processes restart 
+    -- at the beginning once they have reached the final statement.
+    P_Clock: process
+    begin
+        s_Clock <= '0';
+        wait for gCLK_HPER;
+        s_Clock <= '1';
+        wait for gCLK_HPER;
+    end process;   
+
+    -- TestBench Process
+    P_TB: process
+    begin
+        
+        -- Reset
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '1';
+        s_IF_WE <= '0';
+
+        s_ID_RST <= '1';
+        s_ID_WE <= '0';
+        wait for cCLK_PER;
+
+        -- Write to IF
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '1';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+        -- Do nothing
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '0';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+        -- Write to IF
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '1';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+        -- Write to IF
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '1';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+        -- Write to IF
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '1';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+        -- Write to IF
+        s_IF_Inst <= x"FFFF_FFFF";
+        s_IF_RST <= '0';
+        s_IF_WE <= '1';
+
+        s_ID_RST <= '0';
+        s_ID_WE <= '1';
+        wait for cCLK_PER;
+
+    wait;
+    end process;
+
+end behavior;
